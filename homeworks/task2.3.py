b24c4c78dbff0a2786524dcd66f29117c7ddc3e9
Marusiyagolovina <134440179+Marusiyagolovina@users.noreply.github.com>
2023-06-17 20:47:57 +0300

    Add files via upload

---
+++ homeworks/task2.3.py
@@ -0,0 +1,29 @@
+# Задача 2.3.
+
+# Напишите функцию, которая принимает цифры от 0 до 9 и возвращает значение прописью.
+# Например,
+# switch_it_up(1) -> 'One'
+# switch_it_up(3) -> 'Three'
+# switch_it_up(10000) -> None
+# Использовать условный оператор if-elif-else нельзя!
+
+m = int(input(f'Введите цифру от 0 до 9 и нажмите Enter '))
+
+def switch_it_up(number):
+    
+    switcher = {
+            0: "Zero",
+            1: "One",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+        }
+   
+    print(switcher.get(number))
+
+switch_it_up(m)
